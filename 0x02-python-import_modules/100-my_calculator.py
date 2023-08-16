8bcbae4e1f535414d5950fe1184cda26af27a582
mpfarise <ephraimmpfariseni@gmail.com>
2023-08-16 15:23:16 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Handle basic arthimetic operations."""
+    """Handle basic arithmetic operations."""
     from calculator_1 import add, sub, mul, div
     import sys
 
@@ -10,17 +10,11 @@ if __name__ == "__main__":
         sys.exit(1)
 
     ops = {"+": add, "-": sub, "*": mul, "/": div}
-    operator = sys.argv[2]
-    if operator not in ops:
+    if sys.argv[2] not in list(ops.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    if operator == "/" and b == 0:
-        print("Cannot divide by zero.")
-        sys.exit(1)
-
-    result = ops[operator]{a, b)
-    print("{} {} {} = {}".format(a, operator, b, result)
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
