30403ed7791258396b025eec4168cd314c8662ee
mpfarise <ephraimmpfariseni@gmail.com>
2023-08-18 20:24:43 -0700

    102-complex_delete.py

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    list_keys = list(a_dictionary.keys())
+
+    for value_dic in list_keys:
+        if value == a_dictionary.get(value_disc):
+            del a_dictionary[value disc]
+
+        return (a_dictionary)
