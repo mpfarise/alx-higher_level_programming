6b2362cdb1439113fb530da1b6c0ee9f63240874
mpfarise <ephraimmpfariseni@gmail.com>
2023-08-28 14:34:18 -0700

    4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def list_division(my_list_1, my_list_2, list_length):
     """Divides two lists element by element.
 
@@ -25,4 +26,4 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
         finally:
             new_list.append(div)
-        return (new_list)
+    return (new_list)
