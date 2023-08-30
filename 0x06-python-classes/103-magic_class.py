7addcaf6c202ed50849d5c60acee2e997aca8cf4
mpfarise <ephraimmpfariseni@gmail.com>
2023-08-30 12:20:54 -0700

    103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -11,7 +11,7 @@ class MagicClass:
         """Initialize a MagicClass.
 
         Args:
-             radius(float or int): The radius of the new MagicClass.
+            radius(float or int): The radius of the new MagicClass.
         """
         self.__radius = 0
         if type(radius) is not float:
@@ -23,5 +23,5 @@ class MagicClass:
         return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
-        """Return the circumferenceo of the MagicClass."""
+        """Return The circumference of the MagicClass."""
         return (2 * math.pi * self.__radius)
