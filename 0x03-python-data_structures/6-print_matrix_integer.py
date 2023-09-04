a2b99bcc6acfcb22518f7ec88d83126364b52b44
Ephraim Mpfariseni Mphaphuli <ephraimmpfariseni@gmail.com>
2023-09-04 10:23:57 +0200

    Delete 0x03-python-data_structures directory

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for col_index, col in enumerate(row):
-            print("{:d}"format(col), end=" " if col_index != len(row) -1 else "")
-        print()
